b3f1dcf1f17069d5ca766fd5d4c6cb4c40e49a4f
ketankmr <ketank619@gmail.com>
2019-02-17 15:52:30 +0530

    Completed

--- app/src/main/java/ketank/bloodbank/Activity/BankMainActivity.java
+++ app/src/main/java/ketank/bloodbank/Activity/BankMainActivity.java
@@ -1,8 +1,10 @@
 package ketank.bloodbank.Activity;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.astuetz.PagerSlidingTabStrip;
 
@@ -12,6 +14,7 @@ import ketank.bloodbank.R;
 public class BankMainActivity extends AppCompatActivity {
     ViewPager viewPager;
     PagerSlidingTabStrip tabsStrip;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,13 @@ public class BankMainActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
 
         tabsStrip.setViewPager(viewPager);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
 
